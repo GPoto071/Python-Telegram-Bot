97298329113d5b15abf7c82273e30ec5d56c8b99
GPoto <ggpoto37@gmail.com>
2025-08-09 14:16:46 +0300

    Update main.py
    conn.close() removed

--- Telegram_bot/main.py
+++ Telegram_bot/main.py
@@ -1,11 +1,8 @@
 from connections import conn, bot
 from message_handlers import *
 
-conn.close()
-
 if conn:
     print("Подключено")
-
 
 @bot.message_handler(commands=['start'])
 def main(message):
@@ -19,5 +16,6 @@ def main(message):
         bot.send_message(message.chat.id,
                          f'Привет, {message.from_user.first_name}. Этот бот разработан специально для изучения английского языка. Начнём?',
                          reply_markup=markup)
+
 
 bot.polling(non_stop=True)
